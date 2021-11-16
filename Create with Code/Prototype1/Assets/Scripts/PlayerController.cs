68955017191fdf1e5a0f5935d1fecb5a86e38923
zhyq <1064639871@qq.com>
2021-11-16 19:32:34 +0800

    Junior Programmer: Create with Code 1 Programming Basics
    Junior Programmer: Create with Code 1
    Programming Basics

---
+++ Create with Code/Prototype1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 5.0f;
+
+    private float turnSpeed = 0.0f;
+    
+    private float horizontalInput = 0.0f;
+
+    private float forwardInput = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward*Time.deltaTime*speed*forwardInput);
+        transform.Rotate(Vector3.up, Time.deltaTime*turnSpeed*horizontalInput);
+        //transform.Translate(Vector3.right*Time.deltaTime*turnSpeed*horizontalInput);
+    }
+}
