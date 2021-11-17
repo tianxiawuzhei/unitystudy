39660fec1435ef0ef655f8a3a24061bdcadfed4c
zhyq <1064639871@qq.com>
2021-11-17 11:15:22 +0800

    TutorialChallenge 1 - Plane Programming
    https://learn.unity.com/tutorial/lab-1-personal-project-plan?uv=2019.4&pathwayId=5f7e17e1edbc2a5ec21a20af&missionId=5f71fe63edbc2a00200e9de0&projectId=5caccdfbedbc2a3cef0efe63

---
+++ Create with Code/Challenge1/Assets/Scripts/PlanePropeller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanePropeller : MonoBehaviour
+{
+    public GameObject propeller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        propeller.transform.Rotate(Vector3.forward*50*Time.deltaTime);
+    }
+}
