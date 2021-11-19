b0ddfa1d0eaa5bb924c9452297074bbd9bd2d6a1
zhyq <1064639871@qq.com>
2021-11-19 15:27:17 +0800

    Challenge2_PlayFetch: 修改bug，完成教程

--- Create with Code/Challenge2_PlayFetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Create with Code/Challenge2_PlayFetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,40 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    
+    private float shootTime = 0;　　//表示子弹的生成时间间隔 用来控制子弹的发射间隔
 
+    private float shootTimerInterval = 0.5f;　　//表示子弹的间隔这个是一个固定的时间
+
+    private bool can_shoot = true;
+    
     // Update is called once per frame
     void Update()
     {
+        shootTime += Time.deltaTime;　　//让子弹的时间控制器不断加等时间间隔
+
+        if (shootTime > shootTimerInterval)
+        {
+            //如果子弹发射的时间间隔超过时间控制器　　那么我们就发射子弹
+
+            shootTime = 0; //让子弹的时间间隔回复到初始的情况下
+
+            can_shoot = true;
+        }
+
+        Shoot();
+    }
+
+    void Shoot()
+    {
+        if (!can_shoot)
+            return;
         // On spacebar press, send dog
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            can_shoot = false;
+            shootTime = 0;
         }
     }
 }
--- Create with Code/Challenge2_PlayFetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Create with Code/Challenge2_PlayFetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -16,6 +16,7 @@ public class SpawnManagerX : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        float spawnInterval = Random.Range(3, 6);
         InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
     }
 
