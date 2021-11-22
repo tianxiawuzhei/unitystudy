276eaca44311eeaa8e1e81f42fb8db0a3a287145
zhyq <1064639871@qq.com>
2021-11-22 15:27:00 +0800

    Lesson 3.1 - Jump Force

---
+++ Create with Code/Unit3-SoundEffects/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+
+    public float jumpForce = 10;
+
+    public float gravityMotify;
+
+    public bool isOnGround = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityMotify;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up*jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        isOnGround = true;
+    }
+}
